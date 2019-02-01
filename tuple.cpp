8745a88f798ecce7317ed799f9210d98af464be8
Yang <pourplusquoi@icloud.com>
2019-02-01 19:54:17 +0800

    Extend get

--- tuple.cpp
+++ tuple.cpp
@@ -67,10 +67,9 @@ struct tuple_element<0, ::tuple<Head, Tail...>> {
    using type = Head;
 };
 
-template <
-    std::size_t I,
-    typename... Types>
-constexpr typename tuple_element<I, ::tuple<Types...>>::type& get(::tuple<Types...>& t) noexcept {
+template <std::size_t I, typename... Types>
+constexpr typename tuple_element<I,
+    ::tuple<Types...>>::type& get(::tuple<Types...>& t) noexcept {
     return get<I - 1>(t.next);
 }
 
@@ -83,12 +82,51 @@ constexpr Head& get(::tuple<Head, Tail...>& t) noexcept {
     return t.elem;
 }
 
+template <typename Target, typename... Types>
+struct contains;
+
+template <
+    typename Target,
+    typename Head,
+    typename... Tail>
+struct contains<Target, Head, Tail...> {
+    static constexpr bool value = std::is_same<Target, Head>::value
+        || ::contains<Target, Tail...>::value;
+};
+
+template <typename Target, typename Last>
+struct contains<Target, Last> {
+    static constexpr bool value = std::is_same<Target, Last>::value;
+};
+
+template <
+    typename T,
+    typename Head,
+    typename... Tail,
+    typename = std::enable_if_t<
+        std::is_same<T, Head>::value &&
+        !::contains<T, Tail...>::value>>
+constexpr Head& get(::tuple<Head, Tail...>& t) noexcept {
+    return t.elem;
+}
+
+template <
+    typename T,
+    typename Head,
+    typename... Tail,
+    typename = std::enable_if_t<
+        !std::is_same<T, Head>::value &&
+        ::contains<T, Tail...>::value>>
+constexpr T& get(::tuple<Head, Tail...>& t) noexcept {
+    return get(t.next);
+}
+
 int main() {
     const char* hello = "hello";
     tuple<int, double, std::string> elem = ::make_tuple(3, 2.0, hello);
     tuple<int, tuple<int, double, std::string>> t(9, elem);
 
-    std::cout << ::get<0>(t) << std::endl;
+    std::cout << ::get<0>(t) << ", " << ::get<int>(t) << std::endl;
     std::cout << ::get<0>(::get<1>(t)) << ", ";
     std::cout << ::get<1>(::get<1>(t)) << ", ";
     std::cout << ::get<2>(::get<1>(t)) << std::endl;
