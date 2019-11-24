6bf56e9dfe07ca2c1d72e8493d6cf6828b5c1fa5
Yang <pourplusquoi@icloud.com>
2019-11-23 23:04:38 -0800

    Update prime.cc

--- source/prime.cc
+++ source/prime.cc
@@ -4,23 +4,40 @@
 #include <iostream>
 #include <map>
 #include <unordered_map>
-#include <utility>
 
-namespace prime {
+class Prime {
+ public:
+  static int64_t SumUpto(const int64_t n) noexcept;
+  static int64_t NumUpto(const int64_t n) noexcept;
+
+ private:
+ // Calculates the sum of the list $[2..n]$ after filtering out composite
+  // numbers using all the primes ${p' | p' <= p}$.
+  static int64_t PrimeSum(const int64_t n, int64_t p) noexcept;
+
+  // Calculates the # of primes in the list $[2..n]$ after filtering out
+  // composite numbers using all the primes ${p' | p' <= p}$.
+  static int64_t PrimeNum(const int64_t n, int64_t p) noexcept;
+
+  // Get the prime number $p$ s.t. $p <= n$ and there is no prime $p'$
+  // satisfying $p < p' <= n$.
+  static int64_t PreviousPrime(int64_t n) noexcept;
+
+  using Memoization =
+      std::unordered_map<
+          int64_t, std::unordered_map<int64_t, int64_t>>;
+
+  static std::map<int64_t, int64_t> previous_prime_;
+  static Memoization dp_sum_;
+  static Memoization dp_num_;
+};
+
 namespace {
-
-using Memoization =
-    std::unordered_map<
-        int64_t, std::unordered_map<int64_t, int64_t>>;
-
-static std::map<int64_t, int64_t> prevp;
-static Memoization dp_sum, dp_num;
-
-inline int64_t quick_sqrt(const int64_t n) noexcept {
+inline int64_t Sqrt(const int64_t n) noexcept {
   return static_cast<int64_t>(std::sqrt(n));
 }
 
-constexpr int64_t quick_pow(int64_t base, int64_t power,
+constexpr int64_t QuickPower(int64_t base, int64_t power,
                             const int64_t mod) noexcept {
   int64_t res = 1;
   while (power > 0) {
@@ -33,7 +50,7 @@ constexpr int64_t quick_pow(int64_t base, int64_t power,
   return res;
 }
 
-constexpr bool is_prime(const int64_t n) noexcept {
+constexpr bool IsPrime(const int64_t n) noexcept {
   int power = 0;
   int64_t odd = n - 1;
   // Refactor s.t. $n = 2^power * odd$ where $odd = 1 mod 2$.
@@ -45,7 +62,7 @@ constexpr bool is_prime(const int64_t n) noexcept {
   const int upper =
       std::min(n - 2, static_cast<int64_t>(std::floor(2 * lnn * lnn)));
   for (int i = 2; i <= upper; i++) {
-    int64_t val = quick_pow(i, odd, n);
+    int64_t val = QuickPower(i, odd, n);
     if (val == 1 || val == n - 1) {
       continue;
     }
@@ -62,43 +79,30 @@ constexpr bool is_prime(const int64_t n) noexcept {
   }
   return true;
 }
+}  // namespace
 
-// Get the prime number $p$ s.t. $p <= n$ and there is no prime $p'$
-// satisfying $p < p' <= n$.
-int64_t prev_prime(int64_t n) noexcept {
-  if (n > 2 && n % 2 == 0) {
-    n--;
-  }
+std::map<int64_t, int64_t> Prime::previous_prime_;
 
-  const auto iter = prevp.lower_bound(n);
-  if (iter != prevp.cend() && iter->second <= n) {
-    return iter->second;
-  }
+Prime::Memoization Prime::dp_sum_;
+Prime::Memoization Prime::dp_num_;
 
-  int64_t m = n;
-  while (!is_prime(m)) {
-    m -= 2;
-    const auto it = prevp.find(m);
-    if (it != prevp.cend()) {
-      prevp.emplace(n, it->second);
-      prevp.erase(it);
-    }
-  }
-  prevp.emplace(n, m);
-  return m;
+int64_t Prime::SumUpto(const int64_t n) noexcept {
+  return PrimeSum(n, Sqrt(n));
 }
 
-// Calculates the sum of the list $[2..n]$ after filtering out composite
-// numbers using all the primes ${p' | p' <= p}$.
-int64_t prime_sum(const int64_t n, int64_t p) noexcept {
+int64_t Prime::NumUpto(const int64_t n) noexcept {
+  return PrimeNum(n, Sqrt(n));
+}
+
+int64_t Prime::PrimeSum(const int64_t n, int64_t p) noexcept {
   if (n < p * p) {
-    p = quick_sqrt(n);
+    p = Sqrt(n);
   }
   if (p > 1) {
-    p = prev_prime(p);
+    p = PreviousPrime(p);
   }
 
-  auto& entry = dp_sum[n];
+  auto& entry = dp_sum_[n];
   const auto iter = entry.find(p);
   if (iter != entry.cend()) {
     return iter->second;
@@ -110,24 +114,22 @@ int64_t prime_sum(const int64_t n, int64_t p) noexcept {
     return val;
   } else {
     const int64_t val =
-        prime_sum(n, p - 1) -
-            p * (prime_sum(n / p, p - 1) - prime_sum(p - 1, p - 1));
+        PrimeSum(n, p - 1) -
+            p * (PrimeSum(n / p, p - 1) - PrimeSum(p - 1, p - 1));
     entry[p] = val;
     return val;
   }
 }
 
-// Calculates the # of primes in the list $[2..n]$ after filtering out
-// composite numbers using all the primes ${p' | p' <= p}$.
-int64_t prime_num(const int64_t n, int64_t p) noexcept {
+int64_t Prime::PrimeNum(const int64_t n, int64_t p) noexcept {
   if (n < p * p) {
-    p = quick_sqrt(n);
+    p = Sqrt(n);
   }
   if (p > 1) {
-    p = prev_prime(p);
+    p = PreviousPrime(p);
   }
 
-  auto& entry = dp_num[n];
+  auto& entry = dp_num_[n];
   const auto iter = entry.find(p);
   if (iter != entry.cend()) {
     return iter->second;
@@ -138,27 +140,40 @@ int64_t prime_num(const int64_t n, int64_t p) noexcept {
     return n - 1;
   } else {
     const int64_t val =
-        prime_num(n, p - 1) -
-            (prime_num(n / p, p - 1) - prime_num(p - 1, p - 1));
+        PrimeNum(n, p - 1) -
+            (PrimeNum(n / p, p - 1) - PrimeNum(p - 1, p - 1));
     entry[p] = val;
     return val;
   }
 }
 
-}  // namespace
+// Get the prime number $p$ s.t. $p <= n$ and there is no prime $p'$
+// satisfying $p < p' <= n$.
+int64_t Prime::PreviousPrime(int64_t n) noexcept {
+  if (n > 2 && n % 2 == 0) {
+    n--;
+  }
 
-int64_t prime_sum_upto(const int64_t n) noexcept {
-  return prime_sum(n, quick_sqrt(n));
+  const auto iter = previous_prime_.lower_bound(n);
+  if (iter != previous_prime_.cend() && iter->second <= n) {
+    return iter->second;
+  }
+
+  int64_t m = n;
+  while (!IsPrime(m)) {
+    m -= 2;
+    const auto it = previous_prime_.find(m);
+    if (it != previous_prime_.cend()) {
+      previous_prime_.emplace(n, it->second);
+      previous_prime_.erase(it);
+    }
+  }
+  previous_prime_.emplace(n, m);
+  return m;
 }
-
-int64_t prime_num_upto(const int64_t n) noexcept {
-  return prime_num(n, quick_sqrt(n));
-}
-
-}  // namespace prime
 
 int main() {
-    std::cout << prime::prime_sum_upto(1'000'000'000ll) << std::endl;
-    std::cout << prime::prime_num_upto(1'000'000'000ll) << std::endl;
+    std::cout << Prime::SumUpto(1'000'000'000ll) << std::endl;
+    std::cout << Prime::NumUpto(1'000'000'000ll) << std::endl;
     return 0;
 }
