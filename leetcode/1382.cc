db138d35668ad0f938a134268654a036f5a1a54e
Yang Pan <pourplusquoi@icloud.com>
2020-03-17 01:38:30 -0700

    Update 1382.cc

--- leetcode/1382.cc
+++ leetcode/1382.cc
@@ -24,3 +24,32 @@ public:
         traverse(node->right, preorder);
     }
 };
+
+class SolutionEx {
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<TreeNode*> preorder;
+        [&](TreeNode* node) {
+            const auto traverse = [&](TreeNode* node, const auto& ref) {
+                if (node == nullptr)
+                    return;
+                ref(node->left, ref);
+                preorder.push_back(node);
+                ref(node->right, ref);
+            };
+            traverse(node, traverse);
+        }(root);
+        return [&](int lo, int hi) {
+            const auto rebuild = [&](int lo, int hi, const auto& ref) -> TreeNode* {
+                if (lo > hi)
+                    return nullptr;
+                int mid = lo + (hi - lo) / 2;
+                TreeNode* node = preorder[mid];
+                node->left = ref(lo, mid - 1, ref);
+                node->right = ref(mid + 1, hi, ref);
+                return node;
+            };
+            return rebuild(lo, hi, rebuild);
+        }(0, preorder.size() - 1);
+    }
+};
