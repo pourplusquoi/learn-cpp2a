e372a6f7d8b4030b719df2e665653df3b92d36a2
Yang Pan <pourplusquoi@icloud.com>
2020-03-18 16:07:12 -0700

    Create 1373.cc

---
+++ leetcode/1373.cc
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct Result {
+  bool is_bst;
+  int total;
+  int max_sum;
+  int min_val;
+  int max_val;
+};
+
+class Solution {
+public:
+  int maxSumBST(TreeNode* root) {
+    return traverse(root).max_sum;
+  }
+  
+  Result traverse(TreeNode* node) {
+    if (node == nullptr)
+      return Result {
+          .is_bst = true,
+          .total = 0,
+          .max_sum = 0,
+          .min_val = INT_MAX,
+          .max_val = INT_MIN,
+      };
+    auto lhs = traverse(node->left);
+    auto rhs = traverse(node->right);
+    if (valid(node, lhs, rhs)) {
+      int total = node->val + lhs.total + rhs.total;
+      return Result {
+          .is_bst = true,
+          .total = total,
+          .max_sum = std::max(total, std::max(lhs.max_sum, rhs.max_sum)),
+          .min_val = std::min(node->val, lhs.min_val),
+          .max_val = std::max(node->val, rhs.max_val),
+      };
+    } else {
+      return Result {
+          .is_bst = false,
+          .total = node->val,
+          .max_sum = std::max(lhs.max_sum, rhs.max_sum),
+          .min_val = node->val,
+          .max_val = node->val,
+      };
+    }
+  }
+  
+  inline constexpr bool valid(TreeNode* node,
+                              const Result& lhs,
+                              const Result& rhs) {
+    return lhs.is_bst && rhs.is_bst
+        && (node->left == nullptr || lhs.max_val < node->val)
+        && (node->right == nullptr || rhs.min_val > node->val);
+  }
+};
