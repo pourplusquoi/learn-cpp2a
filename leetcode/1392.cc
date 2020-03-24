c0c82fd908ab1614603d1c8352981f6bdbc52f8d
Yang Pan <pourplusquoi@icloud.com>
2020-03-24 01:05:30 -0700

    Update 1392.cc

--- leetcode/1392.cc
+++ leetcode/1392.cc
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_DP {
 public:
   string longestPrefix(const string& s) {
     // KMP...
@@ -13,5 +13,36 @@ public:
       next[i + 1] = j;
     }
     return s.substr(0, next[n]);
+  }
+};
+
+class Solution_RollingHash {
+public:
+  static constexpr int mod = 1'000'000'007;
+  
+  string longestPrefix(const string& s) {
+    int n = s.size();
+    int longest = 0;
+    uint64_t pref = 0, suff = 0;
+    for (int lo = 0, hi = n - 1; hi > 0; lo++, hi--) {
+      pref = ((pref << 8) + s[lo]) % mod;
+      suff = (suff + s[hi] * pow(256, lo)) % mod;
+      if (pref == suff)
+        longest = lo + 1;
+    }
+    return s.substr(0, longest);
+  }
+  
+  uint64_t pow(uint64_t num, int ord) {
+    uint64_t base = num;
+    uint64_t res = 1;
+    while (ord > 0) {
+      if (ord & 1) {
+        res = (res * base) % mod;
+      }
+      ord >>= 1;
+      base = (base * base) % mod;
+    }
+    return res;
   }
 };
