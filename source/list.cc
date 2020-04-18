308d9a43297c0a4eb1880179b3bf27d795e67b91
Yang <pourplusquoi@icloud.com>
2020-04-18 05:09:32 -0700

    Update list.cc

--- source/list.cc
+++ source/list.cc
@@ -34,9 +34,13 @@ template <typename... T>
 constexpr List<Decay<T>...> create_list(T&&... vals);
 
 template <typename Head, typename... Rest,
-          typename Return = typename CreateList<Decay<Head>, Decay<Rest>...>::Type>
+          typename Return = typename CreateList<Decay<Head>,
+                                                Decay<Rest>...>::Type>
 constexpr Return create_list(Head&& head, Rest&&... rest) {
-  return Return { std::forward<Head>(head), create_list(std::forward<Rest>(rest)...) };
+  return Return {
+    std::forward<Head>(head),
+    create_list(std::forward<Rest>(rest)...)
+  };
 }
 
 template <>
@@ -66,15 +70,23 @@ template <typename... T, typename Return = typename CombineLists<T...>::Type>
 constexpr Return concat_lists(T&&... lists);
 
 template <typename Head, typename Tail, typename... Rest,
-          typename Return = typename CombineLists<List<Head, Tail>, Decay<Rest>...>::Type>
+          typename Return = typename CombineLists<List<Head, Tail>,
+                                                  Decay<Rest>...>::Type>
 constexpr Return concat_lists(const List<Head, Tail>& first, Rest&&... rest) {
-  return Return { first.val, concat_lists(first.next, std::forward<Rest>(rest)...) };
+  return Return {
+    first.val,
+    concat_lists(first.next, std::forward<Rest>(rest)...)
+  };
 }
 
 template <typename Head, typename Tail, typename... Rest,
-          typename Return = typename CombineLists<List<Head, Tail>, Decay<Rest>...>::Type>
+          typename Return = typename CombineLists<List<Head, Tail>,
+                                                  Decay<Rest>...>::Type>
 constexpr Return concat_lists(List<Head, Tail>&& first, Rest&&... rest) {
-  return Return { std::move(first.val), concat_lists(std::move(first.next), std::forward<Rest>(rest)...) };
+  return Return {
+    std::move(first.val),
+    concat_lists(std::move(first.next), std::forward<Rest>(rest)...)
+  };
 }
 
 template <typename... Rest,
@@ -99,7 +111,8 @@ struct ReverseList;
 
 template <typename Head, typename Tail>
 struct ReverseList<List<Head, Tail>> {
-  using Type = typename CombineLists<typename ReverseList<Tail>::Type, List<Head, List<>>>::Type;
+  using Type = typename CombineLists<typename ReverseList<Tail>::Type,
+                                     List<Head, List<>>>::Type;
 };
 
 template <>
@@ -118,12 +131,24 @@ constexpr Return reverse_list(const List<Head, Tail>& list) {
 
 template <typename Head, typename Tail,
           typename Return = typename ReverseList<List<Head, Tail>>::Type>
+constexpr Return reverse_list(List<Head, Tail>& list) {
+  return concat_lists(reverse_list(list.next), create_list(list.val));
+}
+
+template <typename Head, typename Tail,
+          typename Return = typename ReverseList<List<Head, Tail>>::Type>
 constexpr Return reverse_list(List<Head, Tail>&& list) {
-  return concat_lists(reverse_list(std::move(list.next)), create_list(std::move(list.val)));
+  return concat_lists(reverse_list(std::move(list.next)),
+                      create_list(std::move(list.val)));
 }
 
 template <>
 constexpr List<> reverse_list(const List<>& _) {
+  return List<> {};
+}
+
+template <>
+constexpr List<> reverse_list(List<>& _) {
   return List<> {};
 }
 
@@ -133,40 +158,70 @@ constexpr List<> reverse_list(List<>&& _) {
 }
 
 template <template <typename> class Serializer, typename T>
-std::string to_string(T&& list);
+inline std::string to_string(T&& list);
 
 template <template <typename> class Serializer, typename Head, typename Tail>
-std::string to_string(const List<Head, Tail>& list) {
+inline std::string to_string(const List<Head, Tail>& list) {
   Serializer<decltype((list.val))> serializer;
   return serializer(list.val) + " -> " + to_string<Serializer>(list.next);
 }
 
 template <template <typename> class Serializer, typename Head, typename Tail>
-std::string to_string(List<Head, Tail>&& list) {
+inline std::string to_string(List<Head, Tail>& list) {
+  Serializer<decltype((list.val))> serializer;
+  return serializer(list.val) + " -> " + to_string<Serializer>(list.next);
+}
+
+template <template <typename> class Serializer, typename Head, typename Tail>
+inline std::string to_string(List<Head, Tail>&& list) {
   Serializer<decltype(std::move(list.val))> serializer;
-  return serializer(std::move(list.val)) + " -> " + to_string<Serializer>(std::move(list.next));
+  return serializer(std::move(list.val)) + " -> " +
+      to_string<Serializer>(std::move(list.next));
 }
 
 template <template <typename> class Serializer>
-std::string to_string(const List<>& _) {
+inline std::string to_string(const List<>& _) {
   return "null";
 }
 
 template <template <typename> class Serializer>
-std::string to_string(List<>&& _) {
+inline std::string to_string(List<>& _) {
+  return "null";
+}
+
+template <template <typename> class Serializer>
+inline std::string to_string(List<>&& _) {
   return "null";
 }
 
 template <typename T>
-struct Serializer {
-  std::string operator()(T&& val) const {
-    return std::to_string(val);
+struct DefaultSerializer {
+  inline std::string operator()(T&& val) const {
+    return std::to_string(std::forward<T>(val));
+  }
+};
+
+template <>
+struct DefaultSerializer<const std::string&> {
+  inline std::string operator()(const std::string& val) const {
+    return val;
+  }
+};
+
+template <>
+struct DefaultSerializer<std::string&&> {
+  inline std::string operator()(std::string&& val) const {
+    return std::move(val);
   }
 };
 
 int main () {
-  constexpr auto list1 = create_list(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
-  constexpr auto list2 = create_list(17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
-  constexpr auto result = concat_lists(list1, list2, reverse_list(list2), reverse_list(list1));
-  std::cout << to_string<Serializer>(result) << std::endl;
+  constexpr auto list1 =
+      create_list(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+  auto list2 = create_list(std::string("hello"),
+                           std::string("world"),
+                           std::string("!"));
+  auto result = concat_lists(list1, list2,
+                             reverse_list(list2), reverse_list(list1));
+  std::cout << to_string<DefaultSerializer>(std::move(result)) << std::endl;
 }
