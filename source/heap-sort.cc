28795977a8c982a947b75f5eb02e6f17d2914a40
Yang <pourplusquoi@icloud.com>
2020-04-19 21:34:22 -0700

    T&& nums

--- source/heap-sort.cc
+++ source/heap-sort.cc
@@ -8,8 +8,11 @@ constexpr void swap(T& a, T& b) {
   b = std::move(c);
 }
 
-template <typename T, std::size_t N, typename Compare = std::less<T>>
-constexpr void swim(std::array<T, N>& nums, std::size_t idx) {
+template <
+    typename T,
+    typename E = decltype(std::declval<T>()[0]),
+    typename Compare = std::less<E>>
+constexpr void swim(T&& nums, std::size_t idx) {
   Compare lt;
   while (idx > 0) {
     auto par = (idx - 1) / 2;
@@ -21,8 +24,11 @@ constexpr void swim(std::array<T, N>& nums, std::size_t idx) {
   }
 }
 
-template <typename T, std::size_t N, typename Compare = std::less<T>>
-constexpr void sink(std::array<T, N>& nums, std::size_t end) {
+template <
+    typename T,
+    typename E = decltype(std::declval<T>()[0]),
+    typename Compare = std::less<E>>
+constexpr void sink(T&& nums, std::size_t end) {
   Compare lt;
   auto idx = 0;
   while (idx < end) {
@@ -41,21 +47,28 @@ constexpr void sink(std::array<T, N>& nums, std::size_t end) {
   }
 }
 
-template <typename T, std::size_t N, typename Compare = std::less<T>>
-constexpr void make_heap(std::array<T, N>& nums) {
-  for (auto i = 0; i < N; i++) {
-    swim(nums, i);
+template <
+    typename T,
+    typename E = decltype(std::declval<T>()[0]),
+    typename Compare = std::less<E>>
+constexpr void make_heap(T&& nums) {
+  for (auto i = 0; i < nums.size(); i++) {
+    swim(std::forward<T>(nums), i);
   }
 }
 
-template <typename T, std::size_t N, typename Compare = std::less<T>>
-constexpr decltype(auto) heap_sort(std::array<T, N> nums) {
-  make_heap(nums);
-  auto sorted = nums;
-  for (std::size_t i = 0, j = N - 1; i < N; i++, j--) {
-    sorted[i] = std::move(nums[0]);
-    nums[0] = std::move(nums[j]);
-    sink(nums, j);
+template <
+    typename T,
+    typename E = decltype(std::declval<T>()[0]),
+    typename Compare = std::less<E>>
+constexpr decltype(auto) heap_sort(T&& nums) {
+  auto heap = nums;
+  make_heap(heap);
+  auto sorted = heap;
+  for (std::size_t i = 0, j = heap.size() - 1; i < heap.size(); i++, j--) {
+    sorted[i] = std::move(heap[0]);
+    heap[0] = std::move(heap[j]);
+    sink(heap, j);
   }
   return sorted;
 }
@@ -63,8 +76,8 @@ constexpr decltype(auto) heap_sort(std::array<T, N> nums) {
 int main() {
   constexpr auto input =
       std::array{28, 5, 24, 13, 2, 14, 30, 23, 4, 10, 21, 25, 29, 1, 12, 16,
-                 7, 6, 26, 3, 15, 31, 18, 7, 20, 0, 22, 19, 11, 17, 27, 9};
-  constexpr auto output = heap_sort(input);
+                 7, 6, 26, 3, 15, 31, 18, 8, 20, 0, 22, 19, 11, 17, 27, 9};
+  constexpr auto output = heap_sort(std::move(input));
   for (const auto& num : output) {
     std::cout << num << std::endl;
   }
