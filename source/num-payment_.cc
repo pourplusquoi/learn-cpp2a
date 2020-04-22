aaf1afe507a1ca139312aec5731282cc520be8ae
Yang Pan <pourplusquoi@icloud.com>
2020-04-21 22:44:42 -0700

    Update num-payment_.cc

--- source/num-payment_.cc
+++ source/num-payment_.cc
@@ -2,31 +2,29 @@
 #include <type_traits>
 #include <chrono>
 
-using namespace std;
-
 template <int ...Currency>
 struct NumPayment;
 
 template <int First, int ...Rest>
 struct NumPayment<First, Rest...> {
-    
+
     template <int Index, typename = void>
-    struct Result : integral_constant<
-        size_t,
+    struct Result : std::integral_constant<
+        long long,
         NumPayment<Rest...>::template Result<Index>::value +
             Result<std::max(Index - First, -1)>::value> {};
 
     template <typename T>
-    struct Result<-1, T> : integral_constant<size_t, 0> {};
+    struct Result<-1, T> : std::integral_constant<long long, 0> {};
 };
 
 template <>
 struct NumPayment<> {
     template <int Index, typename = void>
-    struct Result : integral_constant<long long, 0> {};
+    struct Result : std::integral_constant<long long, 0> {};
 
     template <typename T>
-    struct Result<0, T> : integral_constant<long long, 1> {};
+    struct Result<0, T> : std::integral_constant<long long, 1> {};
 };
 
 int main() {
