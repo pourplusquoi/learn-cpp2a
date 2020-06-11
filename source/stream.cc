7bf7e5a4b637b7390db547902b43904536e96e2b
Yang Pan <pourplusquoi@icloud.com>
2020-06-10 22:24:50 -0700

    Update stream.cc

--- source/stream.cc
+++ source/stream.cc
@@ -2,61 +2,62 @@
 #include <string>
 #include <vector>
 
-template <template<typename> class Container, typename Elem,
+template <template <typename> class Container, typename Elem,
           typename ElemDecay = std::remove_reference_t<Elem>>
 void push(Container<ElemDecay>& container, Elem&& elem);
 
-template <typename Elem,
-          typename ElemDecay = std::remove_reference_t<Elem>>
+template <typename Elem, typename ElemDecay = std::remove_reference_t<Elem>>
 void push(std::vector<ElemDecay>& container, Elem&& elem) {
-    container.push_back(elem);
+  container.push_back(elem);
 }
 
-template <typename F>
+template <typename Fn>
 struct Filter {
-    F f;
-    Filter(F&& f) : f(std::move(f)) {}
+  Fn fn;
+  Filter(Fn&& fn) : fn(std::move(fn)) {}
 
-    template <template<typename> class Container, typename Elem>
-    Container<Elem> operator()(const Container<Elem>& container) {
-        Container<Elem> ret;
-        for (auto&& elem : container) {
-            if (f(elem)) {
-                push(ret, elem);
-            }
-        }
-        return ret;
+  template <template <typename> class Container, typename Elem>
+  Container<Elem> operator()(const Container<Elem>& container) {
+    Container<Elem> ret;
+    for (auto&& elem : container) {
+      if (fn(elem)) {
+        push(ret, elem);
+      }
     }
+    return ret;
+  }
 };
 
-template <typename F>
+template <typename Fn>
 struct Transform {
-    F f;
-    Transform(F&& f) : f(std::move(f)) {}
+  Fn fn;
+  Transform(Fn&& fn) : fn(std::move(fn)) {}
 
-    template <template<typename> class Container, typename ElemIn,
-              typename ElemOut = decltype(std::declval<F>()(std::declval<ElemIn>()))>
-    Container<ElemOut> operator()(const Container<ElemIn>& container) {
-        Container<ElemOut> ret;
-        for (auto&& elem : container) {
-            push(ret, f(elem));
-        }
-        return ret;
+  template <
+      template <typename> class Container, typename ElemIn,
+      typename ElemOut = decltype(std::declval<Fn>()(std::declval<ElemIn>()))>
+  Container<ElemOut> operator()(const Container<ElemIn>& container) {
+    Container<ElemOut> ret;
+    for (auto&& elem : container) {
+      push(ret, fn(elem));
     }
+    return ret;
+  }
 };
 
-template <template<typename> class Container, typename ElemIn, typename Operation>
+template <template <typename> class Container, typename ElemIn,
+          typename Operation>
 auto operator|(const Container<ElemIn>& container, Operation&& operation) {
-    return operation(container);
+  return operation(container);
 }
 
 int main() {
-    std::vector<int> num { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-    auto res = num
-        | Filter([](int v) { return v % 2 == 0; })
-        | Transform([](int v) { return "%" + std::to_string(v * v); });
+  std::vector<int> num{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  auto res = num
+      | Filter([](int v) { return v % 2 == 0; })
+      | Transform([](int v) { return "%" + std::to_string(v * v); });
 
-    for (auto&& v : res) {
-        std::cout << v << std::endl;
-    }
+  for (auto&& v : res) {
+    std::cout << v << std::endl;
+  }
 }
